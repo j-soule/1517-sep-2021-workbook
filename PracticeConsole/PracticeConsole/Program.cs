a6bb4ccf955b68958430d3cb09214271cd514185
Jennifer Soule <jensoule@hotmail.com>
2021-09-19 15:33:10 -0600

    create new practice console project

---
+++ PracticeConsole/PracticeConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console; //fully qualified namespace
+
+namespace PracticeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //System.Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
+        }
+    }
+}
