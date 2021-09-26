d38dcbe9298133d6ed37d6b21a882bf2807f593d
Jennifer Soule <jensoule@hotmail.com>
2021-09-26 17:14:23 -0600

    update class notes

--- PracticeConsole/PracticeConsole/Utilities.cs
+++ PracticeConsole/PracticeConsole/Utilities.cs
@@ -8,5 +8,41 @@ namespace PracticeConsole
 {
     class Utilities
     {
+        
+        //these types of class should not hold and retain data
+        // "static" makes only one active copy of the class 
+        //      when the program runs`  
+        //when using a static class, the developer does not create
+        //   an individual instance, instead the  developer will
+        //   reference items in the static class by ClassName.Method()
+        public static bool IsEmpty(string value)
+        {
+            bool valid = false;
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                valid = true;
+            }
+            return valid;
+        }
+
+        //overload
+        public static bool IsPositive(double value)
+        {
+            bool valid = false;
+            if (value >= 0.0)
+            {
+                valid = true;
+            }
+            return valid;
+        }
+        public static bool IsPositive(int value)
+        {
+            bool valid = false;
+            if (value >= 0)
+            {
+                valid = true;
+            }
+            return valid;
+        }
     }
 }
