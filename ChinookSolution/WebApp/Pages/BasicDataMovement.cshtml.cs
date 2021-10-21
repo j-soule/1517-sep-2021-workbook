e977b33eb6909ce5d6b818d34aeb0c524bfea7ca
Jennifer Soule <jensoule@hotmail.com>
2021-10-20 20:40:28 -0600

    play with onPost and post page handlers, retain data in control

--- ChinookSolution/WebApp/Pages/BasicDataMovement.cshtml.cs
+++ ChinookSolution/WebApp/Pages/BasicDataMovement.cshtml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,13 +14,20 @@ namespace WebApp.Pages
         public string MyName;
 
         //properties
+        [TempData]
+
+        public string FeedbackMessage { get; set; }
+        [BindProperty]
+
+        public int? id { get; set; } //? makes it a nullable feed, it doesnt have to exist
 
         //constructors
 
         //behaviours aka methods
         public void OnGet()
         {
-            //Deafult Request is OnFet(),
+            //events (event driven logic)
+            //Deafult Request event is OnGet(),
             //executed each time the page is entered and or refreshed
             //if no form post with a RedirecToPage
             Random rdn = new Random();
@@ -32,6 +40,52 @@ namespace WebApp.Pages
             {
                 MyName = null;
             }
+        }
+        
+        //Request method:
+        //handles post methods from forms
+        //request obj goes ito code, 
+        //public IActionResult OnPost()
+        //{
+        //    Thread.Sleep(2000);
+        //    string buttonvalue = Request.Form["theButton"];
+        //    if (buttonvalue.Equals("A"))
+        //    {
+        //        //use an asp page handler to get to the logic for this
+        //        //true path
+        //    }
+        //    else if (buttonvalue.Equals("B"))
+        //    {
+
+        //    }
+        //    FeedbackMessage = buttonvalue;
+        //    //the RedirectToPage will cause the OnGet to execute
+        //    return RedirectToPage();
+        //}
+
+        //if you have a handler on the submit button and name your event method using OnPostxxxx where xxxx is the asp-page-handler
+        //then this method will be executed instead of the general OnPost
+
+        //this method (event) logic is dedicated to the action required by the pressed button
+         public IActionResult OnPostAButton()
+        {
+            Thread.Sleep(1000);
+           // string buttonvalue = Request.Form["theButton"];
+            FeedbackMessage = $"You pressed the the A button, input was {id}";
+            //the RedirectToPage will cause the OnGet to execute
+            return RedirectToPage();
+
+        }
+
+
+        public IActionResult OnPostBButton()
+        {
+            Thread.Sleep(1000);
+            // string buttonvalue = Request.Form["theButton"];
+            FeedbackMessage = $"You pressed the B button, input was {id}";
+            //the RedirectToPage will cause the OnGet to execute
+            return RedirectToPage();
+
         }
     }
 }
